e25bfd6f507c75df9a7e1df93a0d759c772035f8
Schtritoff <tstritof@gmail.com>
2023-12-23 23:38:30 +0100

    cleanup script will clean metadata iso also from custom vmstorage path

--- Cleanup-VM.ps1
+++ Cleanup-VM.ps1
@@ -27,17 +27,43 @@ if ($Force -or $PSCmdlet.ShouldContinue("Are you sure you want to delete VM?", "
 
             # remove snapshots
             Remove-VMSnapshot -VMName $v -IncludeAllChildSnapshots -ErrorAction SilentlyContinue
+
+            # disks can be on custom locations and there will also be leftover metadata ISO
+            $vmDiskStoragePath = @()
+
             # remove disks
             Get-VM $v -ErrorAction SilentlyContinue | ForEach-Object {
                 $_.id | get-vhd -ErrorAction SilentlyContinue | ForEach-Object {
+                    $vmDiskStoragePath += (Get-Item $_.path).DirectoryName
                     remove-item -path $_.path -force -ErrorAction SilentlyContinue
                 }
             }
+
             #remove cloud-init metadata iso
             $VHDPath = (Get-VMHost).VirtualHardDiskPath
             Remove-Item -Path "$VHDPath$v-metadata.iso" -ErrorAction SilentlyContinue
+            $vmDiskStoragePath | ForEach-Object {
+                Write-Verbose "Deleting... $_\$v-metadata.iso"
+                Remove-Item -Path "$_\$v-metadata.iso" -ErrorAction SilentlyContinue
+            }
+
             # remove vm
             Remove-VM -VMName $v -Force -ErrorAction SilentlyContinue | Out-Null
+
+            # remove empty directories - src: https://stackoverflow.com/a/28631669/1155121
+            $vmDiskStoragePath | ForEach-Object {
+                # this should be your -VMMachine_StoragePath
+                $topDir = (Get-Item $_).Parent.Parent.FullName
+                Write-Verbose "Deleting empty folders from top path: $topDir"
+                do {
+                    $dirs = Get-ChildItem $topDir -directory -recurse | Where-Object { (Get-ChildItem $_.fullName).count -eq 0 } | Select-Object -expandproperty FullName
+                    $dirs | Foreach-Object {
+                        Write-Verbose "Removing... $_"
+                        Remove-Item $_ -Force:$false -ErrorAction SilentlyContinue
+                    }
+                } while ($dirs.count -gt 0)
+            }
+
         }
 
         Write-Host -ForegroundColor Green " Done."
